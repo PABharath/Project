4ee0cf03288cc5c1eeebbd0a7aadaf34f5cd6dd8
Bharath <sonubuji6@gmail.com>
2024-09-06 10:54:18 +0530

    Bharath commit

--- src/bharath/Day3.java
+++ src/bharath/Day3.java
@@ -3,16 +3,18 @@ package bharath;
 public class Day3 {
 
 	public static void main(String[] args) {
-	int	count=0;
+
 		int num=46;
 		for(int i=0;i<num;i++) {
 			if(i%2==0) {
 				System.out.println("Even number is "+i);
-				count++;
-				
+							
+			}else if(i%3==0) {
+			
+				System.out.println("odd number"+i);
 			}
 		}
-		System.out.println(count);
+		
 
 	}
 
